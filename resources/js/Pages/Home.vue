e6355af8c8b5b6ffa30418700f6ee5c51466ea9b
KatyHummes <katielihummes15@gmail.com>
2023-10-15 21:19:55 -0300

    install jetstream

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+    <div class="container mx-auto xl:px-20">
+
+        <div class="min-h-screen flex justify-center items-center">
+            
+        </div>
+
+    </div>
+</template>
